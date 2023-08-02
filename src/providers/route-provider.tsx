4f90931f0982156aaa1279cc0777b98e6df8045e
Malika Rakhimova <malikarakhimova31@gmail.com>
2023-08-02 12:05:00 +0500

    bug fix

--- src/providers/route-provider.tsx
+++ src/providers/route-provider.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import routes from "@/routes/routes";
 
 export default function RouteProvider(): React.ReactElement {
-  const router = createBrowserRouter(routes);
+  // const router = createBrowserRouter(routes);
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <RouterProvider router={router} />
+      <RouterProvider router={routes} />
     </Suspense>
   );
 }
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -14,11 +14,212 @@ import Polyclinic from "@/features/polylinic";
 import RecipeRecommendation from "@/features/recipe-recommendation";
 import IssueHistory from "@/features/issue-history";
 import AuthCallback from "@/features/auth/views/modal/AuthCallback";
+import { createBrowserRouter } from "react-router-dom";
 
-const routes: CustomRoute[] = [
+// const routes: CustomRoute[] = [
+//   {
+//     id: "root",
+//     title: "ERecipe",
+//     element: <Root />,
+//     loader: async () => null,
+//     errorElement: <div>Error element</div>,
+//     children: [
+//       {
+//         id: "create-recipe",
+//         path: "/create-recipe",
+//         element:
+//           import.meta.env.VITE_ROLE === roles.doctor ? (
+//             <CreateRecipe />
+//           ) : (
+//             <NoPermission
+//               title="У вас нет доступа к созданию рецепта"
+//               text="Ваши права не позволяют Вам создать рецепт. Выписать рецепты пациенты могут только врачи.  "
+//             />
+//           ),
+//         loader: async () => null,
+//         errorElement: <div>Error element</div>,
+//       },
+//       {
+//         id: "doctors",
+//         path: "/doctors",
+//         element:
+//           import.meta.env.VITE_ROLE === roles.mainDoctor ||
+//           import.meta.env.VITE_ROLE === roles.minzdrav ? (
+//             <Doctors />
+//           ) : (
+//             <NoPermission
+//               title="У вас нет доступа к созданию рецепта"
+//               text="Ваши права не позволяют Вам создать рецепт. Выписать рецепты пациенты могут только врачи.  "
+//             />
+//           ),
+//         loader: async () => null,
+//         errorElement: <div>Error element</div>,
+//       },
+//       {
+//         id: "erecipes",
+//         path: "/erecipes",
+//         element:
+//           import.meta.env.VITE_ROLE !== roles.doctor ? (
+//             <ERecipes />
+//           ) : (
+//             <NoPermission
+//               title="У вас нет доступа к созданию рецепта"
+//               text="Ваши права не позволяют Вам создать рецепт. Выписать рецепты пациенты могут только врачи.  "
+//             />
+//           ),
+//         loader: async () => null,
+//         errorElement: <div>Error element</div>,
+//       },
+//       {
+//         id: "erecipes-recipe-version",
+//         path: "/erecipes/recipe-version/:id",
+//         element:
+//           import.meta.env.VITE_ROLE !== roles.doctor ? (
+//             <RecipeVersion />
+//           ) : (
+//             <NoPermission
+//               title="У вас нет доступа к созданию рецепта"
+//               text="Ваши права не позволяют Вам создать рецепт. Выписать рецепты пациенты могут только врачи.  "
+//             />
+//           ),
+//         loader: async () => null,
+//         errorElement: <div>Error element</div>,
+//       },
+//       {
+//         id: "patients",
+//         path: "/patients",
+//         element:
+//           import.meta.env.VITE_ROLE !== roles.pharmacy ? (
+//             <PatientsHome />
+//           ) : (
+//             <NoPermission
+//               title="У вас нет доступа к созданию рецепта"
+//               text="Ваши права не позволяют Вам создать рецепт. Выписать рецепты пациенты могут только врачи.  "
+//             />
+//           ),
+//         loader: async () => null,
+//         errorElement: <div>Error element</div>,
+//       },
+//       {
+//         id: "patient-recipe-history",
+//         path: "/patients/:id",
+//         element:
+//           import.meta.env.VITE_ROLE !== roles.pharmacy ? (
+//             <RecipeHistory />
+//           ) : (
+//             <NoPermission
+//               title="У вас нет доступа к созданию рецепта"
+//               text="Ваши права не позволяют Вам создать рецепт. Выписать рецепты пациенты могут только врачи.  "
+//             />
+//           ),
+//         loader: async () => null,
+//         errorElement: <div>Error element</div>,
+//       },
+//       {
+//         id: "patient-recipe-version",
+//         path: "/patients/recipe-version/:id",
+//         element:
+//           import.meta.env.VITE_ROLE !== roles.pharmacy ? (
+//             <RecipeVersion />
+//           ) : (
+//             <NoPermission
+//               title="У вас нет доступа к созданию рецепта"
+//               text="Ваши права не позволяют Вам создать рецепт. Выписать рецепты пациенты могут только врачи.  "
+//             />
+//           ),
+//         loader: async () => null,
+//         errorElement: <div>Error element</div>,
+//       },
+//       {
+//         id: "patient-recipe-edit",
+//         path: "/patients/recipe-edit",
+//         element:
+//           import.meta.env.VITE_ROLE === "DOCTOR" ? (
+//             <CreateRecipe />
+//           ) : (
+//             <NoPermission
+//               title="У вас нет доступа к созданию рецепта"
+//               text="Ваши права не позволяют Вам создать рецепт. Выписать рецепты пациенты могут только врачи.  "
+//             />
+//           ),
+//         loader: async () => null,
+//         errorElement: <div>Error element</div>,
+//       },
+//       {
+//         id: "dashboard",
+//         path: "/",
+//         element:
+//           import.meta.env.VITE_ROLE !== roles.pharmacy ? (
+//             <Dashboard />
+//           ) : (
+//             <NoPermission
+//               title="У вас нет доступа к созданию рецепта"
+//               text="Ваши права не позволяют Вам создать рецепт. Выписать рецепты пациенты могут только врачи.  "
+//             />
+//           ),
+//         index: true,
+//         loader: async () => null,
+//         errorElement: <div>Error element</div>,
+//       },
+//       {
+//         id: "polyclinic",
+//         path: "/polyclinics",
+//         element:
+//           import.meta.env.VITE_ROLE === roles.minzdrav ? (
+//             <Polyclinic />
+//           ) : (
+//             <NoPermission
+//               title="У вас нет доступа к созданию рецепта"
+//               text="Ваши права не позволяют Вам создать рецепт. Выписать рецепты пациенты могут только врачи.  "
+//             />
+//           ),
+//         index: true,
+//         loader: async () => null,
+//         errorElement: <div>Error element</div>,
+//       },
+//       {
+//         id: "issue-history",
+//         path: "/issue-history",
+//         element:
+//           import.meta.env.VITE_ROLE === roles.pharmacy ? (
+//             <IssueHistory />
+//           ) : (
+//             <NoPermission
+//               title="У вас нет доступа к созданию рецепта"
+//               text="Ваши права не позволяют Вам создать рецепт. Выписать рецепты пациенты могут только врачи.  "
+//             />
+//           ),
+//         index: true,
+//         loader: async () => null,
+//         errorElement: <div>Error element</div>,
+//       },
+//     ],
+//   },
+//   {
+//     id: "recipe-recommendation",
+//     title: "Pharmacy recipe recommendation",
+//     path: "/recipe-recommendation/:id",
+//     element: <RecipeRecommendation />,
+//   },
+//   authRoutes,
+//   {
+//     id: "test",
+//     title: "test",
+//     path: "auth/callback",
+//     element: <AuthCallback />,
+//     children: [],
+//   },
+//   {
+//     id: "global-not-found",
+//     title: "Not found",
+//     path: "*",
+//     element: <h1>404: Page not found</h1>,
+//   },
+// ];
+
+const routes = createBrowserRouter([
   {
     id: "root",
-    title: "ERecipe",
     element: <Root />,
     loader: async () => null,
     errorElement: <div>Error element</div>,
@@ -196,24 +397,21 @@ const routes: CustomRoute[] = [
   },
   {
     id: "recipe-recommendation",
-    title: "Pharmacy recipe recommendation",
     path: "/recipe-recommendation/:id",
     element: <RecipeRecommendation />,
   },
   authRoutes,
   {
     id: "test",
-    title: "test",
     path: "auth/callback",
     element: <AuthCallback />,
     children: [],
   },
   {
     id: "global-not-found",
-    title: "Not found",
     path: "*",
     element: <h1>404: Page not found</h1>,
   },
-];
+]);
 
 export default routes;
